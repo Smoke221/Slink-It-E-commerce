bcc20678784867287c5a992bbd70f252bdaf4772
Anil <kanilreddy867@gmail.com>
2022-11-13 20:56:45 +0530

    project done

--- cart.js
+++ cart.js
@@ -1,7 +1,5 @@
 let cartItems = JSON.parse(localStorage.getItem("cartproducts")) || []
-let count = 0;
-console.log(cartItems);
-console.log(count)
+
 displayCard(cartItems)
 
 let totalCost = cartItems.reduce((acc,e) => acc+e.price, 0)
@@ -10,7 +8,7 @@ document.querySelector(".total-mrp").innerText = totalCost
 
 let discount = cartItems.reduce((acc,e) => acc + e.discount, 0)
 console.log(discount)
-document.querySelector(".total-discount").innerText = discount;
+document.querySelector(".total-discount").innerText = "-" + discount;
 
 
 let totalAmount = totalCost - discount
@@ -41,14 +39,23 @@ function displayCard(data) {
         name.innerText = ele.name
 
         let price = document.createElement("p")
-        price.innerText = ele.price
+        price.innerText = "₹" + ele.price
 
-        let removeButton = document.createElement("button")
-        removeButton.setAttribute("class", "remove-button")
-        removeButton.innerText = "REMOVE"
+        // let removeButton = document.createElement("button")
+        // removeButton.setAttribute("class", "remove-button")
+        // removeButton.innerText = "REMOVE"
 
-        let divs4 = document.createElement("divs")
-        divs4.se
+        let divs4 = document.createElement("divs");
+        divs4.setAttribute("id","qty")
+
+        let quantity = document.createElement("p")
+        quantity.innerText = ele.quantity
+        let minusbtn = document.createElement("button")
+        minusbtn.setAttribute("id","minus")
+        minusbtn.innerText = "-"
+        let plusbtn = document.createElement("button")
+        plusbtn.setAttribute("id","plus")
+        plusbtn.innerText = "+"
 
         let line = document.createElement("hr")
 
@@ -56,24 +63,36 @@ function displayCard(data) {
         divs.append(divs2, divs3)
         divs2.append(image);
         divs3.append(name, price)
-        document.querySelector(".cart-items").append(divs, removeButton, line);
+        divs4.append(minusbtn,quantity,plusbtn)
+        document.querySelector(".cart-items").append(divs, divs4, line);
     })
 } 
 
 let removeCartItems = document.getElementsByClassName("remove-button");
 //console.log(removeCartItems)
 
-for(let i=0; i<removeCartItems.length; i++){
-    let btn = removeCartItems[i]
-    btn.addEventListener("click",function(event){
-        let buttonClicked = event.target;
-        buttonClicked.parentElement.remove()
-        updateCart();
-    })
-}
+// for(let i=0; i<removeCartItems.length; i++){
+//     let btn = removeCartItems[i]
+//     btn.addEventListener("click",function(event){
+//         let buttonClicked = event.target
+//         buttonClicked.parentElement.remove(i)
+//         updateCart();
+//     })
+// }
 
 let orderButton = document.querySelector(".order-button")
 orderButton.addEventListener("click", function(){
     window.location.href="payment.html"
 })
 
+
+
+let minusbtn = document.querySelector("#minus")
+minusbtn.addEventListener("click",function(){
+    console.log("working")
+})
+
+let plusbtn = document.querySelector("#plus")
+plusbtn.addEventListener("click",function(){
+    console.log("yess")
+})
